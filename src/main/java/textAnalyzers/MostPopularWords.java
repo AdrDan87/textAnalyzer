cf0bdb7f2eda41607589592d7cc0d3a24afbff56
Adrian Danielak <adrian.danielak87@gmail.com>
2018-12-15 15:44:58 +0100

    class MostPopularWords roughly done and tests pass

--- src/main/java/textAnalyzers/MostPopularWords.java
+++ src/main/java/textAnalyzers/MostPopularWords.java
@@ -1,8 +1,22 @@
 package textAnalyzers;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class MostPopularWords implements Analyser {
     @Override
     public String analyze(String text) {
-        return null;
+        String[] toLowerCaseOnlyLetters = text.toLowerCase().replaceAll("[^a-z]+", " ").split(" ");
+
+        Map<String, Long> output = Arrays.stream(toLowerCaseOnlyLetters).collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+
+        List<Map.Entry<String,Long>> result = output.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(10).collect(Collectors.toList());
+        StringBuffer out = new StringBuffer();
+        for (int i = 0; i < result.size(); i++) {
+            out.append(result.get(i));
+            out.append(' ');
+        }
+        return out.toString();
+        }
     }
-}
+
--- src/test/java/MostPopularWordsTest.java
+++ src/test/java/MostPopularWordsTest.java
@@ -1,2 +1,28 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import textAnalyzers.Analyser;
+import textAnalyzers.MostPopularWords;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
 public class MostPopularWordsTest {
+
+
+    @Test
+    @Parameters(method = "mostPopularWordsValues")
+    public void testCorrectCharFreq(String input, String expected) {
+        Analyser popular = new MostPopularWords();
+        assertEquals(expected, popular.analyze(input));
+    }
+
+
+    private Object[] mostPopularWordsValues() {
+        return new Object[]{
+                new Object[]{"la ka ma la ka", "la=2 ka=2 ma=1 "},
+                new Object[]{"la ka ma la ka laaaa", "la=2 ka=2 laaaa=1 ma=1 "},
+        };
+    }
 }
